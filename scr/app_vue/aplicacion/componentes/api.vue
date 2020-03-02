d9e43ba98e8f7834ef2cbf062b8dea8c76e396ec
Jorge Meza <jorge.meza.rdz@gmail.com>
2020-03-02 04:47:31 -0600

    inicio

--- scr/app_vue/aplicacion/componentes/api.vue
+++ scr/app_vue/aplicacion/componentes/api.vue
@@ -1,14 +1,11 @@
 <template>
-<div>
-   <app-video v-if="rutas.video"  :direccionVideo="rutaVideo"></app-video>
-  <app-inicio v-if="rutas.inicio"></app-inicio>
-</div>
-
+  <div>
+    <app-video v-if="rutas.video" :direccionVideo="rutaVideo"></app-video>
+  </div>
 </template>
    
 <script>
-
-import io from 'socket.io-client';
+import io from "socket.io-client";
 import video from "./video.vue";
 import inicio from "./inicio.vue";
 export default {
@@ -16,91 +13,78 @@ export default {
   data() {
     return {
       rutas: {
-        video:true,
-        inicio:false
+        video: true
       },
-      socket:"",
-      rutaVideo:"/videos/v1_atocha_sub_low.mp4"
+      socket: "",
+      rutaVideo: "/videos/v1_atocha_sub_low.mp4"
     };
   },
   computed: {},
-  methods: {
-    
-  },
+  methods: {},
   mounted: function() {
-    
-    
-    this.socket=io.connect('http://'+window.location.host, { 'forceNew': true })
-this.socket.on('messages', function(data) {
+    this.socket = io.connect("http://" + window.location.host, {
+      forceNew: true
+    });
+    this.socket.on(
+      "messages",
+      function(data) {
+        console.log(data);
 
-      console.log(data);
-     
+        if (data == "nivel0") {
+          this.rutaVideo = "/videos/v1_atocha_sub_low.mp4";
+        }
 
-    if (data== "nivel0") {
-      this.rutaVideo = "/videos/v1_atocha_sub_low.mp4";
-    }
+        if (data == "nivel1") {
+          this.rutaVideo = "/videos/v2_atocha_sub_low.mp4";
+        }
 
-    if (data == "nivel1") {
-      
-      this.rutaVideo = "/videos/v2_atocha_sub_low.mp4";
-     
-    }
+        if (data == "nivel2") {
+          this.rutaVideo = "/videos/v3_atocha_sub_low.mp4";
+        }
 
-    if (data == "nivel2") {
-      
-      this.rutaVideo = "/videos/v3_atocha_sub_low.mp4";
-     
-    }
-
-    if (data == "nivel3") {
-      
-      this.rutaVideo ="/videos/v4_atocha_sub_low.mp4";
-     
-    }
-
-	
-}.bind(this));
-
-
+        if (data == "nivel3") {
+          this.rutaVideo = "/videos/v4_atocha_sub_low.mp4";
+        }
+      }.bind(this)
+    );
   },
-  created: function() { },
+  created: function() {},
   components: {
-    'app-video':video,
-    'app-inicio':inicio,
+    "app-video": video,
+    "app-inicio": inicio
   }
 };
 </script>
 
 <style scoped>
-
-video { 
-  margin: 0px; 
-  padding: 0px; 
-  height: 100%; 
+video {
+  margin: 0px;
+  padding: 0px;
+  height: 100%;
   border: none;
-  background-color: #000; 
-} 
+  background-color: #000;
+}
 
 video {
-  display: block;       /* iframes are inline by default */
+  display: block; /* iframes are inline by default */
   background: #000;
-  border: none;         /* Reset default border */
-  height: 90vh;        /* Viewport-relative units */
+  border: none; /* Reset default border */
+  height: 90vh; /* Viewport-relative units */
   width: 90vw;
 }
 
-.container{
+.container {
   background-color: #000;
-  height: 100vh; 
+  height: 100vh;
 }
-.row{
-  background-color: #000; 
+.row {
+  background-color: #000;
 }
-.app{
-  background-color: #000; 
+.app {
+  background-color: #000;
 }
 
-.img-class{
+.img-class {
   margin-top: 25%;
   margin-left: 25%;
 }
