1e15dc034dcba8b7effc6efba83e0f41f2e66539
Jorge Meza <jorge.meza.rdz@gmail.com>
2020-03-04 16:31:45 -0600

    04-03-2020-mod

--- server/controladores/dps.js
+++ server/controladores/dps.js
@@ -6,12 +6,21 @@ var LED3 = new Gpio(25, 'out');
 
 var stepPin = new Gpio(16, 'out');
 var dirPin = new Gpio(20, 'out');
-var EnPin = new Gpio(21, 'out');
 
+var EnPin = new Gpio(21, 'out');
+var EnPin1 = new Gpio(4, 'out');
+var EnPin2 = new Gpio(17, 'out');
+var EnPin3 = new Gpio(27, 'out');
 
 var Arriba = new Gpio(5, 'in', 'both');
 var Abajo = new Gpio(6, 'in', 'both');
+
 var FC0 = new Gpio(12, 'in', 'both');
+var FC01 = new Gpio(22, 'in', 'both');
+var FC02 = new Gpio(10, 'in', 'both');
+var FC03 = new Gpio(9, 'in', 'both');
+
+
 var FC1 = new Gpio(13, 'in', 'both');
 var FC2 = new Gpio(19, 'in', 'both');
 var FC3 = new Gpio(26, 'in', 'both');
@@ -21,6 +30,7 @@ var IO;
 
 var puntero = 1;
 var flagAction = false;
+var puntero2=0;
 
 
 
@@ -64,15 +74,98 @@ FC0.watch(function (err, value) {
   }
   if (value == 0 && puntero == 0 && flagAction == true) {
     clearInterval(pulso);
-    flagAction = false;
+    //flagAction = false;
     console.log('FC0');
     IO.emit("messages", "nivel0");
     EnPin.writeSync(1);
     LED1.writeSync(0);
     LED2.writeSync(0);
     LED3.writeSync(0);
+    puntero2 = puntero2 +1;
+  }
+   
+  if(puntero2==4){
+    puntero2 = 0;
+    flagAction = false;
+  }
+
+});
+
+FC01.watch(function (err, value) {
+  if (err) {
+    console.error('There was an error', err);
+    return;
+  }
+  if (value == 0 && puntero == 0 && flagAction == true) {
+    clearInterval(pulso);
+    flagAction = false;
+    console.log('FC01');
+    //IO.emit("messages", "nivel0");
+    EnPin1.writeSync(1);
+    LED1.writeSync(0);
+    LED2.writeSync(0);
+    LED3.writeSync(0);
+    puntero2 = puntero2 +1;
+  }
+
+  if(puntero2==4){
+    puntero2 = 0;
+    flagAction = false;
   }
 });
+
+FC02.watch(function (err, value) {
+  if (err) {
+    console.error('There was an error', err);
+    return;
+  }
+  if (value == 0 && puntero == 0 && flagAction == true) {
+    clearInterval(pulso);
+    flagAction = false;
+    console.log('FC02');
+    //IO.emit("messages", "nivel0");
+    EnPin2.writeSync(1);
+    LED1.writeSync(0);
+    LED2.writeSync(0);
+    LED3.writeSync(0);
+    puntero2 = puntero2 +1;
+  }
+
+  if(puntero2==4){
+    puntero2 = 0;
+    flagAction = false;
+  }
+
+});
+
+FC03.watch(function (err, value) {
+  if (err) {
+    console.error('There was an error', err);
+    return;
+  }
+  if (value == 0 && puntero == 0 && flagAction == true) {
+    clearInterval(pulso);
+    flagAction = false;
+    console.log('FC03');
+    //IO.emit("messages", "nivel0");
+    EnPin3.writeSync(1);
+    LED1.writeSync(0);
+    LED2.writeSync(0);
+    LED3.writeSync(0);
+    puntero2 = puntero2 +1;
+  }
+
+  if(puntero2==4){
+    puntero2 = 0;
+    flagAction = false;
+  }
+
+});
+
+
+
+
+
 
 FC1.watch(function (err, value) {
   if (err) {
@@ -84,6 +177,9 @@ FC1.watch(function (err, value) {
     flagAction = false;
     console.log('FC1');
     EnPin.writeSync(1);
+    EnPin1.writeSync(1);
+    EnPin2.writeSync(1);
+    EnPin3.writeSync(1);
     IO.emit("messages", "nivel1");
     LED1.writeSync(1);
     LED2.writeSync(0);
@@ -100,6 +196,9 @@ FC2.watch(function (err, value) {
     clearInterval(pulso);
     flagAction = false;
     EnPin.writeSync(1);
+    EnPin1.writeSync(1);
+    EnPin2.writeSync(1);
+    EnPin3.writeSync(1);
     console.log('FC2');
     IO.emit("messages", "nivel2");
     LED1.writeSync(0);
@@ -117,6 +216,9 @@ FC3.watch(function (err, value) {
     clearInterval(pulso);
     flagAction = false;
     EnPin.writeSync(1);
+    EnPin1.writeSync(1);
+    EnPin2.writeSync(1);
+    EnPin3.writeSync(1);
     console.log('FC3');
     IO.emit("messages", "nivel3");
     LED1.writeSync(0);
@@ -131,8 +233,14 @@ process.on('SIGINT', _ => {
   LED3.unexport();
   stepPin.unexport();
   EnPin.unexport();
+  EnPin1.unexport();
+  EnPin2.unexport();
+  EnPin3.unexport();
   dirPin.unexport();
   FC0.unexport();
+  FC01.unexport();
+  FC02.unexport();
+  FC03.unexport();
   FC1.unexport();
   FC2.unexport();
   FC3.unexport();
@@ -147,6 +255,9 @@ function setArriba() {
     console.log('buscando Arriba');
     IO.emit("messages", "buscando Arriba");
     EnPin.writeSync(0);
+    EnPin1.writeSync(0);
+    EnPin2.writeSync(0);
+    EnPin3.writeSync(0);
     pulso = setInterval(_ => stepPin.writeSync(stepPin.readSync() ^ 1), 1);
   }
 }
@@ -159,6 +270,9 @@ function setAbajo() {
   console.log('buscando Abajo');
   IO.emit("messages", "buscando Abajo");
   EnPin.writeSync(0);
+  EnPin1.writeSync(0);
+  EnPin2.writeSync(0);
+  EnPin3.writeSync(0);
   pulso = setInterval(_ => stepPin.writeSync(stepPin.readSync() ^ 1), 1);
  }
 }
